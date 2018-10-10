e16cde4a20f540e265091100d3517c43e7f5a29c
Jonathan Rocha <jonathan.rocha@msn.com>
2018-10-10 18:29:30 -0300

    Servidor reportando mensagens de erro

--- api/controllers/estacionamentoController.js
+++ api/controllers/estacionamentoController.js
@@ -13,13 +13,26 @@ exports.listar_todas_as_vagas = function(req, res) {
 };
 
 exports.criar_vaga = function(req, res) {
-  var new_vaga = new Vaga(req.body);
-  var id = new_vaga.andar + "" + new_vaga.numero;
-  new_vaga.id = id;
-  new_vaga.save(function(err, vaga) {
+  Vaga.find({
+    id: req.body.andar + "" + req.body.numero
+  }, function(err, vaga) {
+    // Checa se houve erros
     if (err)
       res.send(err);
-    res.json(vaga);
+
+    // Checa se a vaga existe
+    if (vaga.length > 0) {
+      res.json("ERRO: Vaga repetida");
+    } else {
+      var new_vaga = new Vaga(req.body);
+      var id = new_vaga.andar + "" + new_vaga.numero;
+      new_vaga.id = id;
+      new_vaga.save(function(err, vaga) {
+        if (err)
+          res.send(err);
+        res.json(vaga);
+      });
+    }
   });
 };
 
@@ -30,7 +43,12 @@ exports.consultar_vaga = function(req, res) {
   }, function(err, vaga) {
     if (err)
       res.send(err);
-    res.json(vaga);
+
+    if (vaga.length > 0) {
+      res.json("ERRO: Vaga inexistente");
+    } else {
+      res.json(vaga);
+    }
   });
 };
 
@@ -46,22 +64,48 @@ exports.deletar_vaga = function(req, res) {
 };
 
 exports.ocupar_vaga = function(req, res) {
-  Vaga.findOneAndUpdate({id: req.params.vagaId}, {
-    ocupada: true, data_ocupacao: new Date(Date.now())  // O que vai ser mudado
-  }, {new: true}, function(err, vaga) {
+  Vaga.find({
+    id: req.params.vagaId
+  }, function(err, vaga) {
+    // Checa se houve erros
     if (err)
       res.send(err);
-    res.json(vaga);
+
+    // Checa se a vaga já está ocupada
+    if (vaga[0].ocupada == true) {
+      res.json("ERRO: Vaga já ocupada");
+    } else {
+      Vaga.findOneAndUpdate({id: req.params.vagaId}, {
+        ocupada: true, data_ocupacao: new Date(Date.now())  // O que vai ser mudado
+      }, {new: true}, function(err, vaga) {
+        if (err)
+          res.send(err);
+        res.json(vaga);
+      });
+    }
   });
 };
 
 exports.desocupar_vaga = function(req, res) {
-  Vaga.findOneAndUpdate({id: req.params.vagaId}, {
-    ocupada: false, data_ocupacao: new Date(Date.now())  // O que vai ser mudado
-  }, {new: true}, function(err, vaga) {
+  Vaga.find({
+    id: req.params.vagaId
+  }, function(err, vaga) {
+    // Checa se houve erros
     if (err)
       res.send(err);
-    res.json(vaga);
+
+    // Checa se a vaga já está ocupada
+    if (vaga[0].ocupada == false) {
+      res.json("ERRO: Vaga já desocupada");
+    } else {
+      Vaga.findOneAndUpdate({id: req.params.vagaId}, {
+        ocupada: false, data_ocupacao: new Date(Date.now())  // O que vai ser mudado
+      }, {new: true}, function(err, vaga) {
+        if (err)
+          res.send(err);
+        res.json(vaga);
+      });
+    }
   });
 };
 
@@ -92,14 +136,29 @@ exports.quantidade_vagas_desocupadas = function(req, res) {
 
 exports.tempo_ocupacao = function(req, res) {
   Vaga.find({
-    id: req.params.vagaId   // Parametro da busca
+    id: req.params.vagaId
   }, function(err, vaga) {
+    // Checa se houve erros
     if (err)
       res.send(err);
 
-    var currentDate = new Date(Date.now());
-    var minutos = (currentDate.getTime() - vaga[0].data_ocupacao.getTime()) / 60000;
+    // Checa se a vaga está ocupada
+    if (vaga[0].ocupada == false) {
+      res.json("ERRO: Vaga desocupada");
+    } else {
+      Vaga.find({
+        id: req.params.vagaId   // Parametro da busca
+      }, function(err, vaga) {
+        if (err)
+          res.send(err);
 
-    res.json(Math.floor(minutos));
+        var currentDate = new Date(Date.now());
+        var minutos = (currentDate.getTime() - vaga[0].data_ocupacao.getTime()) / 60000;
+
+        res.json(Math.floor(minutos));
+      });
+    }
   });
+
+
 };
